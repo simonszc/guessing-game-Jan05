e26852508fc66c02056a8c7460eada528b1a63be
Zach Simonson <zsimonson26@gmail.com>
2016-01-08 19:03:24 -0800

    JS cleanup, altered final result

--- app.js
+++ app.js
@@ -2,30 +2,29 @@ var res1 = document.getElementById('resultOne');
 var res2 = document.getElementById('resultTwo');
 var res3 = document.getElementById('resultThree');
 var res4 = document.getElementById('resultFour');
-
+var resFinal = document.getElementById('finalResults');
+var answersRight = 0;
+var timesTried = 0;
 
 var userName = prompt('Hello, friend! What is your name?');
-alert('Greetings, ' + userName + '. My name is Zach. I\'ll ask you a few      questions about me to break the ice.');
+alert('Greetings, ' + userName + '. My name is Zach. I\'ll ask you a few questions about me to break the ice.');
 console.log('User\'s name is ' + userName + '.');
 
-var answersRight = 0;
-
-
-var questData = [['First question,' + userName + '! Am I from Bellevue? Answer Okay for True cancel for False',
+var questData = [['First question, ' + userName + '! Am I from Bellevue? Click "OK" for true, "Cancel" for false',
 false,
  userName + ' got the first question right!',
  'That\'s right! I was born in Redmond.',
  'That\'s not where I\'m from.',
  userName + ' got the first question wrong',
  res1],
- ['Next question,' + userName + '! Did I go to college in Walla Walla? Click OK if you think it\'s true, Cancel if false.',
+ ['Next question,' + userName + '! Did I go to college in Walla Walla? Click "OK" if you think it\'s true, "Cancel" if false.',
  true,
  userName + ' got the second question right!',
   'That\'s right! I went to school over there at Whitman College. Lots of wheat fields. Lots of socially conservative billboards.',
   'Nope, I did',
   userName + ' got the second question wrong',
   res2],
-  ['Next question,' + userName + '! Is my cat\'s name Sophocles? Again, click OK if you think it\'s true, Cancel if false..',
+  ['Next question,' + userName + '! Is my cat\'s name Sophocles? Again, click "OK" if you think it\'s true, "Cancel" if false..',
   false,
   userName + ' got the third question right!',
   'That\'s right! His name is Archimedes. The educated cat.',
@@ -34,68 +33,26 @@ false,
   res3
 ]];
 
-
-
-// function firstQuestion(){
-//   var answerOne = prompt('First question,' + userName + '! Am I from Bellevue? Answer \'Yes\' or \'No\'');
-//   if (answerOne.toUpperCase() === 'NO' || answerOne.toUpperCase() === 'N') {
-//     answersRight++;
-//     console.log(guestData[0][2]);
-//     // alert('That\'s right, ' + userName + '! I was born in Redmond');
-//     res1.textContent = 'That\'s right! I was born in Redmond.';
-//   }
-//   else if (answerOne === 'YES' || answerOne === 'Y') {
-//     // alert('Nope, ' + userName + '. That\'s not where I\'m from.');
-//     res1.textContent = 'That\'s not where I\'m from.';
-//     console.log(userName + ' got the first question wrong');
-//   }
-//   else {
-//     // alert('You need to answer yes or no, ' + userName + '.')
-//       res1.textContent = 'You didn\'t answer Yes or No.';
-//   };
-//   console.log(answersRight + ' answers right.');
-// }
-
-
-
 function askQuestion(){
   var answerTwo = confirm(questData[i][0]);
   if (answerTwo === questData[i][1]) {
-    answersRight++;
-    console.log(questData[i][2]);
-    // alert('That\'s right, ' + userName + '! I went to school over there at Whitman College. Lots of wheat fields. Lots of socially conservative billboards.');
+    // Right Answer
     questData[i][6].textContent  = questData[i][3];
     questData[i][6].className = 'right';
+    answersRight++;
+    console.log(questData[i][2]);
   }
   else {
     console.log(questData[i][5]);
-    // alert('Nope, ' + userName + '. I did.');
+    // Wrong Answer
     questData[i][6].textContent = questData[i][4];
     questData[i][6].className = 'wrong';
   };
   console.log(answersRight + ' answers right.');
 }
-
-
-
-// function thirdQuestion(){
-//   var answerThree = confirm('Next question,' + userName + '! Is my cat\'s name Sophocles? Again, click OK if you think it\'s true, Cancel if false..');
-//   if (answerThree === false) {
-//     answersRight++;
-//     console.log(userName + ' got the third question right!');
-//     // alert('That\'s right, ' + userName + '! His name is Archimedes. The educated cat.');
-//     res3.textContent = 'That\'s right! His name is Archimedes. The educated cat.';
-//   }
-//   else {
-//     console.log(userName + ' got the third question wrong.');
-//     // alert('Nope, ' + userName + '. That\'s not his name. Wise choice on my part. He\'s not a deeply moral creature');
-//     res3.textContent = 'Nope, That\'s not his name. Wise choice on my part. He\'s not a deeply moral creature';
-//   };
-//   console.log(answersRight + ' answers right.');
-// }
-  var timesTried = 0;
-
-
+for (var i = 0; i < questData.length; i++) {
+  askQuestion();
+}
 
 function fourthQuestion(){
   var correctAnswerFour = false;
@@ -104,10 +61,9 @@ function fourthQuestion(){
     console.log(userName + ' guessed my age was ' + answerFour + '.');
     answerFour = parseInt(answerFour);
     if (answerFour === 27) {
-      answersRight++;
       correctAnswerFour = true;
+      answersRight++;
       timesTried++;
-      // alert('That\'s right, ' + userName + '! I\'m 27 years old.');
       res4.textContent = 'Yes, I\'m 27 years old and it took you ' + timesTried + ' tries.';
       res4.className = 'right';
     }
@@ -125,12 +81,9 @@ function fourthQuestion(){
     };
   };
 }
-for (var i = 0; i < questData.length; i++) {
-  askQuestion();
-}
-//firstQuestion();
-//secondQuestion();
-//thirdQuestion();
 fourthQuestion();
+resFinal.textContent = 'Congratulations, ' + userName + '! You got ' + answersRight + ' questions correct! And it only took you ' + timesTried + ' attempts to guess my age.';
 
-alert('Congratulations, ' + userName + '! You got ' + answersRight + ' questions correct! And it only took you ' + timesTried + ' attempts to guess my age.');
+
+
+//alert('Congratulations, ' + userName + '! You got ' + answersRight + ' questions correct! And it only took you ' + timesTried + ' attempts to guess my age.');
